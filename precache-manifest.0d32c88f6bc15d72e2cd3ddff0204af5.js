6f0731913b4b83fb4c90ff0ddbea081f5d1a4766
Benjamin Gabay <benjigabay13@gmail.com>
2022-03-23 13:44:48 -0500

    Updates

---
+++ precache-manifest.0d32c88f6bc15d72e2cd3ddff0204af5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6becf73743da810182",
+    "url": "/ReactPagesTest/static/css/main.323afe84.chunk.css"
+  },
+  {
+    "revision": "9b6becf73743da810182",
+    "url": "/ReactPagesTest/static/js/main.9b6becf7.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/ReactPagesTest/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/ReactPagesTest/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "3b59b3f51186c874189b",
+    "url": "/ReactPagesTest/static/js/runtime~main.3b59b3f5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/ReactPagesTest/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "63bf444f16f0bdce740918b5ba4d515d",
+    "url": "/ReactPagesTest/index.html"
+  }
+];
